ed9c42c2b11f6ee73cdc150bf540a56022339acf
Isaac Whitfield <zackehsoul@hotmail.co.uk>
2013-02-14 15:57:24 +0000

    added class methods

--- src/WeekFour.cpp
+++ src/WeekFour.cpp
@@ -39,6 +39,20 @@ void teddyBear(){
 
 	cout << "rupert's data is : " <<  rupert.height  << " : "
 			<< rupert.colour << endl;
+
+	TeddyBear freddie;
+	freddie.height = 175;
+	freddie.colour = "brown";
+	ShyBear violet;
+	violet.setHeight(175);
+	violet.setColour("yellow");
+
+	cout << "Violet: " << violet.toString() << endl;
+	cout << "Violet's colour is: " << violet.getColour() << endl;
+	cout << "Violet's height is: " << violet.getHeight() << endl;
+
+
+	cout << "Freddie: " << freddie.toString() << endl;
 }
 
 void toddler(){
@@ -54,4 +68,32 @@ void toddler(){
 			<< fred.kiddy.gender << " : "
 			<< fred.bruin.height << " : "
 			<< fred.bruin.colour << endl;
+}
+
+string TeddyBear::toString(){
+	char number[100];
+	sprintf(number, "%g", height);
+	return "Height = " + string(number) + " colour = " + colour;
+}
+
+string ShyBear::getColour(){
+	return colour;
+}
+
+double ShyBear::getHeight(){
+	return height;
+}
+
+void ShyBear::setColour(string colour){
+	ShyBear::colour = colour;
+}
+
+void ShyBear::setHeight(double height){
+	ShyBear::height = height;
+}
+
+string ShyBear::toString(){
+	char number[100];
+	sprintf(number, "%g", height);
+	return "Height = " + string(number) + " colour = " + colour;
 }
