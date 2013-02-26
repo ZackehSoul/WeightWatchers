7c9c47c1c60f2fdd6fee21cc96a7ec0a4b4e1fa8
Isaac Whitfield <zackehsoul@hotmail.co.uk>
2013-02-26 18:04:49 +0000

    remove pointless references

--- src/WeightWatchers.h
+++ src/WeightWatchers.h
@@ -13,33 +13,4 @@ void calculateStatistics();
 void toReturnOrExit();
 string toLowerCase(string &);
 
-struct PersonData {
-	double height;
-	double weight;
-	char gender;
-};
-
-struct TeddyBear {
-  double height;
-  string colour;
-  string toString();
-};
-
-struct Toddler {
-  PersonData kiddy;
-  TeddyBear bruin;
-};
-
-class ShyBear{
-private:
-	double height;
-	string colour;
-public:
-	string getColour();
-	void setColour(string);
-	double getHeight();
-	void setHeight(double);
-	string toString();
-};
-
 #endif
