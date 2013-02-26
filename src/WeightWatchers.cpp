1d45cdfb14a7de5f525c035391ccccb8102da334
Isaac Whitfield <zackehsoul@hotmail.co.uk>
2013-02-26 16:33:19 +0000

    added menu return/exit system

--- src/WeightWatchers.cpp
+++ src/WeightWatchers.cpp
@@ -4,45 +4,57 @@
 
 int main() {
 	int selection;
-	system("CLS");
-	cout << "Welcome to the Healthy Club Command System." << endl;
-	cout << "Please type the number of the feature you wish to use\n" << endl;
-	cout << "1. Calculate your body statistics\n" << endl;
-	cout << "Your selection: "; cin >> selection;
-
-	if(selection == 1){
+	while(exitStatus != "exit"){
 		system("CLS");
-		calculateStatistics();
+		cout << "Welcome to the Healthy Club Command System." << endl;
+		cout << "Please type the number of the feature you wish to use:\n" << endl;
+		cout << "1. Calculate your body statistics\n" << endl;
+		cout << "Your selection: "; cin >> selection;
+
+		if(selection == 1){
+			system("CLS");
+			calculateStatistics();
+		}
 	}
 }
 
 void calculateStatistics(){
 	BodyStatsCalculator * pStats = new BodyStatsCalculator();
-		string memberName; double height; double weight; char gender;
+	string memberName; double height; double weight; char gender;
 
-		cout << "Please enter the required information\n" << endl;
-		Member member;
-		cout << "Please enter the client's first name: "; cin >> memberName;
-		member.setMemberName(memberName);
-		cout << "Please enter the client's height(cm): "; cin >> height;
-		member.setHeight(height);
-		cout << "Please enter the client's weight(kg): "; cin >> weight;
-		member.setWeight(weight);
-		cout << "Please enter the client's gender: "; cin >> gender; cout << endl;
-		member.setGender(gender);
+	cout << "Please enter the required information:\n" << endl;
+	Member member;
+	cout << "Please enter your first name: "; cin >> memberName;
+	member.setMemberName(memberName);
+	cout << "Please enter your height(cm): "; cin >> height;
+	member.setHeight(height);
+	cout << "Please enter your weight(kg): "; cin >> weight;
+	member.setWeight(weight);
+	cout << "Please enter your gender: "; cin >> gender; cout << endl;
+	member.setGender(gender);
 
-		double bmi = pStats->bodyMassIndex(member.getHeight(), member.getWeight());
-		double bsa = pStats->bodySurfaceArea(member.getHeight(), member.getWeight());
-		double lbw = pStats->leanBodyWeight(member.getHeight(), member.getWeight(), member.getGender());
-		string clientGender = pStats->getGender(member.getGender());
-		string bmiResult = pStats->bodyMassResult(bmi, member.getGender());
+	double bmi = pStats->bodyMassIndex(member.getHeight(), member.getWeight());
+	double bsa = pStats->bodySurfaceArea(member.getHeight(), member.getWeight());
+	double lbw = pStats->leanBodyWeight(member.getHeight(), member.getWeight(), member.getGender());
+	string clientGender = pStats->getGender(member.getGender());
+	string bmiResult = pStats->bodyMassResult(bmi, member.getGender());
 
-		system("CLS");
-		cout << "These are your calculated statistics:\n" << endl;
-		cout << "Your Body Mass Index is " << bmi << " which indicates that you are " << bmiResult << endl;
-		cout << "Your Body Surface Area is " << bsa << " square metres." << endl;
-		cout << "Your Lean Body Weight is " << lbw << " kilograms." << endl;
-		cout << clientGender << endl;
+	system("CLS");
+	cout << "These are your calculated statistics:\n" << endl;
+	cout << "Your Body Mass Index is " << bmi << " which indicates that you are " << bmiResult << endl;
+	cout << "Your Body Surface Area is " << bsa << " square metres." << endl;
+	cout << "Your Lean Body Weight is " << lbw << " kilograms." << endl;
+	cout << clientGender << "\n" << endl;
 
-		delete pStats;
+	toReturnOrExit();
+	delete pStats;
+}
+
+void toReturnOrExit(){
+	string selection;
+	cout << "To return to the main menu type \"menu\" or type \"exit\" to exit: "; cin >> selection;
+
+	if(selection == "exit"){
+		exitStatus = "exit";
+	}
 }
