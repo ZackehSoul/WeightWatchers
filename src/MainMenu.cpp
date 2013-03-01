dc2cef6025bb24d4cc537d9f108883bc498a05ee
Isaac Whitfield <zackehsoul@hotmail.co.uk>
2013-03-01 12:09:07 +0000

    added testing of simulation functions

--- src/MainMenu.cpp
+++ src/MainMenu.cpp
@@ -1,3 +1,4 @@
+#include "Trainer.h"
 #include "WeightWatchers.h"
 
 /**
@@ -12,21 +13,26 @@ int main() {
 	WeightWatchers * pMain = new WeightWatchers();
 	// Set the variables needed by the menu
 	int selection; int i = 0;
+
 	// Create a loop which is ended by the user
 	while(pMain->getExitStatus() != "exit"){
 		// Clear the screen and display opening information
-		system("CLS");
+		pMain->clearScreen();
 		cout << "Welcome to the Healthy Club Command System." << endl;
 		cout << "Please type the number of the feature you wish to use:\n" << endl;
-		cout << "1. Calculate your body statistics\n" << endl;
+		cout << "1. Calculate your body statistics" << endl;
+		cout << "2. Start the Client Server simulation\n" << endl;
 		cout << "Your selection: "; cin >> selection;
 
 		// Validates that the user has made a valid selection
 		validateSelection:
 		if(selection == 1){
 			// Clear the screen and continue to generate statistics
-			system("CLS");
+			pMain->clearScreen();
 			pMain->calculateStatistics();
+		} else if (selection == 2){
+			pMain->clearScreen();
+			pMain->runSimulation();
 		} else {
 			// Show the error and ask for a valid selection
 			if (i == 0) cout << endl, i++;
