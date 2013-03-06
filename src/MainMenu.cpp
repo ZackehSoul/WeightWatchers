6e8bdec2d8c59b649431d263971457ded135f0b2
Isaac Whitfield <zackehsoul@hotmail.co.uk>
2013-03-06 00:58:54 +0000

    main menu saves new members and recognises old

--- src/MainMenu.cpp
+++ src/MainMenu.cpp
@@ -1,5 +1,6 @@
 #include "WeightWatchers.h"
 //--- Testing ---//
+#include <fstream>
 #include "LinkedList.h"
 #include "Trainer.h"
 #include <windows.h>
@@ -16,53 +17,59 @@
 int main() {
 	WeightWatchers * pMain = new WeightWatchers();
 	// Set the variables needed by the menu
-	int selection; int i = 0;
+	int selection; int i = 0; string firstName, lastName, gender;
 	// Create a loop which is ended by the user
 	while(pMain->getExitStatus() != "exit"){
 		// Clear the screen and display opening information
 		pMain->clearScreen();
-		cout << "Welcome to the Healthy Club Command System." << endl;
+		cout << "Welcome to the Healthy Club Command System.\n" << endl;
+		cout << "Please type in your first name: "; cin >> firstName;
+		cout << "Please type in your last name: "; cin >> lastName;
+		cout << "Please type in your gender: "; cin >> gender;
+
+		pMain->clearScreen();
+		ifstream memberFile("resources/" + firstName + " " + lastName + ".txt");
+		ofstream members;
+		if(memberFile){
+			cout << "Welcome back, " << firstName << "." << endl;
+		} else {
+			ofstream createFile("resources/" + firstName + " " + lastName + ".txt");
+			members.open("members.txt", ios_base::app);
+			members << firstName << " " << lastName << endl;
+			members.close();
+			cout << "Welcome to the club, " << firstName << "." << endl;
+		}
 		cout << "Please type the number of the feature you wish to use:\n" << endl;
-		cout << "1. Calculate your body statistics" << endl;
-		cout << "2. Start the Client Server simulation\n" << endl;
+		cout << "1. Start the Client Server simulation" << endl;
+		cout << "2. Calculate your body statistics\n" << endl;
 		cout << "Your selection: "; cin >> selection;
 
 		// Validates that the user has made a valid selection
 		validateSelection:
 		if(selection == 1){
-			// Clear the screen and continue to generate statistics
-			pMain->clearScreen();
-			pMain->calculateStatistics();
-		} else if (selection == 2){
 			// Clear the screen and initialize the simulation
 			pMain->clearScreen();
 			pMain->runSimulation();
+		} else if (selection == 2){
+			// Clear the screen and continue to generate statistics
+			pMain->clearScreen();
+			pMain->calculateStatistics(firstName + " " + lastName, gender);
 		} else if (selection == 0000){
 			//--- Test function ---//
-			int trainerNo;
-			// Asks for the number of trainers
-			cout << "Please enter an amount of trainers: "; cin >> trainerNo;
-			// Creates that amount of trainer objects
-			Trainer trainers[trainerNo];
-			LinkedList pList;
-			for(int i = 0; i < trainerNo; i++){
-				// Give all trainers an ID number
-				trainers[i].setTrainerID(i + 1);
-				// Add them to the linked list
-				pList.addTrainerElement(&trainers[i]);
+			vector<string> lines;
+			ifstream myfile("members.txt");
+			if(myfile.is_open()){
+				string line;
+				while(getline(myfile, line))
+				{
+					lines.push_back(line);
+				}
 			}
-			// Check if they added to the list correctly
-			cout << pList.listElements() << endl;
-			int busyNess;
-			// Make some of them busy
-			cout << "Please enter the amount you want to make busy: "; cin >> busyNess;
-			for(int i = 0; i < busyNess; i++){
-				// Remove the busy trainers from the list
-				pList.removeTrainerElement();
+			for(unsigned int i = 0; i < lines.size(); i++){
+				cout << lines.at(i) << endl;
 			}
-			// Check they removed correctly
-			cout << pList.listElements();
-			Sleep(10000);
+			cout << lines.size();
+			Sleep(60000);
 			//--- Test function ---//
 		} else {
 			// Show the error and ask for a valid selection
